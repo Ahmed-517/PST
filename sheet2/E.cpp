47d71ece07b52f29845122cf2262b4ca50be01cc
Ahmed-517 <ahmed195041@feng.bu.edu.eg>
2022-09-25 05:07:49 +0200

    Problem E. Keyboard

---
+++ sheet2/E.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+    char dir; cin >> dir;
+    string word; cin >> word;
+    vector<char>output;
+
+    string keyboard = "qwertyuiopasdfghjkl;zxcvbnm,./";
+
+    if (dir == 'R') {
+
+        for (int i=0; i<word.size(); i++) {
+            for (int j=0; j<keyboard.size(); j++) {
+                if (word[i] == keyboard[j])
+                    output.push_back(keyboard[j-1]);
+            }
+        }
+
+    } else if (dir == 'L') {
+
+        for (int i=0; i<word.size(); i++) {
+            for (int j=0; j<keyboard.size(); j++) {
+                if (word[i] == keyboard[j])
+                    output.push_back(keyboard[j+1]);
+            }
+        }
+
+    }
+
+    for (auto i : output) cout << i;
+}
